783d2e070930dd591510992ed8cc1644b6cdd69b
José Victor Farias de Oliveira <jose.oliveira@compasso.com.br>
2021-04-25 22:53:28 -0300

    feat: adicionado schema de jogador ao módulo de jogadores

--- src/jogadores/jogadores.module.ts
+++ src/jogadores/jogadores.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresController } from './jogadores.controller';
 import { JogadoresService } from './jogadores.service';
+import { JogadorSchema } from './schemas/jogador.schema';
 
 @Module({
+  imports: [
+    /* registro do schema JogadoSchema dentro do módulo Jogadores, passando 
+       deixando disponível uma instancia do objeto dentro desse escopo 
+       (singleton)
+    */
+    MongooseModule.forFeature([
+        {
+          name: 'Jogador', 
+          schema: JogadorSchema
+        }
+      ])
+    ],
   controllers: [JogadoresController],
   providers: [JogadoresService]
 })
