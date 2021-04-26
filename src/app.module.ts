4f4a858a2828f0547b99811416305aee5747f06b
José Victor Farias de Oliveira <jose.oliveira@compasso.com.br>
2021-04-25 22:51:56 -0300

    chore: adicionado configurações de env e mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
-
+import constantsDefault from './constants/default'
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    JogadoresModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot( process.env.MONGODB_URL, constantsDefault.config.mongodb )
+  ],
   controllers: [],
   providers: [],
 })
